3f1d5ece7125bce32eb5713dcea22e91a3975be3
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-05-19 12:18:02 -0300

    robin

--- Automatos/SimuladorDFA/main.py
+++ Automatos/SimuladorDFA/main.py
@@ -4,7 +4,6 @@ DFA Simulator:
         Enrico Giannobile RA: 19.00610-0
         Guilherme de Campos RA: 20.00089-8
         Luis Guilherme de Souza Munhoz RA: 20.01937-8
-
 '''
 
 def simulate_dfa(dfa, input):
