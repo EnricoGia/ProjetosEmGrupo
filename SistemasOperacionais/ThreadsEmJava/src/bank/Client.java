183057e8678dc2f67c866719bedc2d11d24afff8
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-05-19 12:25:36 -0300

    consertando o source

--- SistemasOperacionais/ThreadsEmJava/src/bank/Client.java
+++ SistemasOperacionais/ThreadsEmJava/src/bank/Client.java
@@ -1,48 +1,46 @@
 package bank;
 
-
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Client extends Thread{
+public class Client extends Thread {
     private Account account;
-    public final Integer[] values = {10,20,50,100};
+    public final Integer[] values = { 10, 20, 50, 100 };
 
-    public Client(String name, Account account){
+    public Client(String name, Account account) {
         super(name);
         setAccount(account);
     }
 
     public void execute() throws InterruptedException {
-        Integer action = ThreadLocalRandom.current().nextInt(0,2);      
-        Integer value = values[ThreadLocalRandom.current().nextInt(0,4)];
-        if(action ==  1){
-           account.deposit(value);
-        } else if(action == 0){
+        Integer action = ThreadLocalRandom.current().nextInt(0, 2);
+        Integer value = values[ThreadLocalRandom.current().nextInt(0, 4)];
+        if (action == 1) {
+            account.deposit(value);
+        } else if (action == 0) {
             account.withdraw(value);
         }
     }
 
     @Override
     public void run() {
-        try{
-            while(true){
+        try {
+            while (true) {
                 this.execute();
                 Thread.yield();
             }
         }
-            
+
         catch (InterruptedException e) {
-                
+
         }
 
-        
     }
+
     public Account getAccount() {
         return account;
     }
 
-
     public void setAccount(Account account) {
         this.account = account;
     }
-       }
+}
